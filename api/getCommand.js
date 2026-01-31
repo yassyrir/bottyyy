d806e321c3a659b9b0e79c05065af6aad2236362
yassyrir <ytojjulayto@gmail.com>
2026-01-31 18:36:38 +0700

    Create getCommand.js

---
+++ api/getCommand.js
@@ -0,0 +1,9 @@
+let currentCommand = 'CHECK';
+
+export default function handler(req, res) {
+  if (req.method === 'GET') {
+    res.status(200).json({ command: currentCommand });
+  } else {
+    res.status(405).send('Method Not Allowed');
+  }
+}
